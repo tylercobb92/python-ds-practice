9dc55f00a8af1e19a82b76db33df4260bf5e7dd2
tyler <tylercobb92@gmail.com>
2021-11-26 16:53:52 -0500

    07 completed

--- 07_multiple_letter_count/multiple_letter_count.py
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,8 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    count = {}
+    for letter in phrase:
+        count[letter] = count.get(letter, 0)+1
+
+    return count
