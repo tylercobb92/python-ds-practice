ce559d0744036b50d7cf65484d376d32d8e481f8
tyler <tylercobb92@gmail.com>
2021-11-26 18:55:15 -0500

    17 completed

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -11,3 +11,11 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+    mode = 0
+
+    for num in nums:
+        n = nums.count(num)
+        if n > mode:
+            mode = num
+
+    return mode
