6ea952d3324fc30fb69f8bb266a000ca6175b483
tyler <tylercobb92@gmail.com>
2021-12-01 15:46:49 -0500

    26 completed

--- 26_vowel_count/vowel_count.py
+++ 26_vowel_count/vowel_count.py
@@ -3,7 +3,15 @@ def vowel_count(phrase):
 
         >>> vowel_count('rithm school')
         {'i': 1, 'o': 2}
-        
+
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+    vowels = ('aeiou')
+    count = {}
+
+    phrase = phrase.lower()
+    for ltr in phrase:
+        if ltr in vowels:
+            count[ltr] = count.get(ltr, 0)+1
+    return count
