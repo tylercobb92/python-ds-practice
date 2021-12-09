4f440726c76527ab4ea8ec71d9ac9d378509624b
tyler <tylercobb92@gmail.com>
2021-12-09 12:40:20 -0500

    36 completed

--- 36_find_the_duplicate/find_the_duplicate.py
+++ 36_find_the_duplicate/find_the_duplicate.py
@@ -13,3 +13,10 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+
+    checked = set()
+
+    for num in nums:
+        if num in checked:
+            return num
+        checked.add(num)
