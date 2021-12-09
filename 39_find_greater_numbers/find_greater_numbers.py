bc860b4b812d8e4645ec53a5c13c0c4029ac5295
tyler <tylercobb92@gmail.com>
2021-12-09 12:52:35 -0500

    39 completed

--- 39_find_greater_numbers/find_greater_numbers.py
+++ 39_find_greater_numbers/find_greater_numbers.py
@@ -19,3 +19,11 @@ def find_greater_numbers(nums):
         >>> find_greater_numbers([])
         0
     """
+    total = 0
+
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[j] > nums[i]:
+                total += 1
+
+    return total
